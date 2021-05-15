a21cbb8211bec68c4d0d54c0925953ba276db3f2
root <root@localhost.localdomain>
2021-05-15 01:40:12 -0400

    page: stock_data

---
+++ vue/src/components/stock_data.vue
@@ -0,0 +1,75 @@
+<template>
+      <!-- Counts Section -->
+  <div>
+      <section class="dashboard-counts">
+        <div class="container-fluid">
+          <div class="row">
+            <!-- Count item widget-->
+            <div class="col-xl-2 col-md-4 col-6">
+              <div class="wrapper count-title d-flex">
+                <div class="icon"><i class="icon-user"></i></div>
+                <div class="name"><strong class="text-uppercase">漲跌幅(%)</strong>
+                  <div class="count-number">25</div>
+                </div>
+              </div>
+            </div>
+            <!-- Count item widget-->
+            <div class="col-xl-2 col-md-4 col-6">
+              <div class="wrapper count-title d-flex">
+                <div class="icon"><i class="icon-padnote"></i></div>
+                <div class="name"><strong class="text-uppercase">開盤價</strong>
+                  <div class="count-number">400</div>
+                </div>
+              </div>
+            </div>
+            <!-- Count item widget-->
+            <div class="col-xl-2 col-md-4 col-6">
+              <div class="wrapper count-title d-flex">
+                <div class="icon"><i class="icon-check"></i></div>
+                <div class="name"><strong class="text-uppercase">收盤價</strong>
+                  <div class="count-number">342</div>
+                </div>
+              </div>
+            </div>
+            <!-- Count item widget-->
+            <div class="col-xl-2 col-md-4 col-6">
+              <div class="wrapper count-title d-flex">
+                <div class="icon"><i class="icon-bill"></i></div>
+                <div class="name"><strong class="text-uppercase">最高價</strong>
+                  <div class="count-number">123</div>
+                </div>
+              </div>
+            </div>
+            <!-- Count item widget-->
+            <div class="col-xl-2 col-md-4 col-6">
+              <div class="wrapper count-title d-flex">
+                <div class="icon"><i class="icon-list"></i></div>
+                <div class="name"><strong class="text-uppercase">最低價</strong>
+                  <div class="count-number">92</div>
+                </div>
+              </div>
+            </div>
+            <!-- Count item widget-->
+            <div class="col-xl-2 col-md-4 col-6">
+              <div class="wrapper count-title d-flex">
+                <div class="icon"><i class="icon-list-1"></i></div>
+                <div class="name"><strong class="text-uppercase">成交量(張)</strong>
+                  <div class="count-number">70</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "StockData"
+}
+</script>
+
+<style scoped src="@/assets/stock_data/css/stock_data.css">
+
+</style>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import UserInfo from '@/components/user_info'
 import Login from '@/components/login'
+import StockData from '@/components/stock_data'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/stockData',
+      name: 'StockData',
+      component: StockData
     },
   ]
 })
